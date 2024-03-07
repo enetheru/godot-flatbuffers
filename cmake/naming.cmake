f48f1d434e0b3f0cc12e273b350699362121a74a
Samuel Nicholas <nicholas.samuel@gmail.com>
2024-03-08 00:36:33 +1030

    Split naming functions into parts to build a godot-exe based on options

--- cmake/naming.cmake
+++ cmake/naming.cmake
@@ -1,6 +1,6 @@
-# Construct the output name
-# godot.<platform>.<target>[.dev][.double].<arch>[.custom_suffix][.console].exe
-function( gde_generate_names )
+# Construct the output names
+
+function( gde_name_arch )
     # Target Architecture
     string( TOLOWER ${CMAKE_SYSTEM_PROCESSOR} GDE_TARGET_ARCH )
     if( GDE_TARGET_ARCH MATCHES "x64|amd64" )
@@ -18,28 +18,72 @@ function( gde_generate_names )
     else()
         set( GDE_TARGET_ARCH ${CMAKE_SYSTEM_PROCESSOR} )
     endif()
+    return( PROPAGATE GDE_TARGET_ARCH )
+endfunction()
+gde_name_arch()
 
+function( gde_name_platform )
     # Target Platform
     string( TOLOWER ${CMAKE_SYSTEM_NAME} GDE_TARGET_PLATFORM ) # AKA Platform
+    return( PROPAGATE GDE_TARGET_PLATFORM )
+endfunction()
+gde_name_platform()
 
-    # Output Filename
-    set(NAME_PARTS ${PROJECT_NAME})
-
-    list(APPEND NAME_PARTS "${GDE_TARGET_PLATFORM}")
-
+function( gde_name_preset )
+    # Target Platform
     if (GEL_TOOLS_ENABLED)
-        list(APPEND NAME_PARTS "editor")
+        set( GDE_TARGET_PRESET "editor")
     elseif (CMAKE_BUILD_TYPE MATCHES "Debug")
-        list(APPEND NAME_PARTS "template_debug")
+        set( GDE_TARGET_PRESET "template_debug")
     else ()
-        list(APPEND NAME_PARTS "template_release")
+        set( GDE_TARGET_PRESET "template_release")
     endif ()
+    return( PROPAGATE GDE_TARGET_PRESET )
+endfunction()
+gde_name_preset()
 
-    if (GEL_DEV_BUILD)
+function( godot_executable_name )
+    # look for godot executable with these names
+    set(NAME_PARTS "godot")
+    list(APPEND NAME_PARTS "${GDE_TARGET_PLATFORM}")
+    list(APPEND NAME_PARTS "editor") # we dont need preset here, we always want the editor
+
+    if (${GEL_DEV_BUILD})
         list(APPEND NAME_PARTS "dev")
     endif ()
 
-    if (GEL_FLOAT_DOUBLE)
+    if(${GEL_FLOAT_DOUBLE})
+        list(APPEND NAME_PARTS "double")
+    endif ()
+
+    list(APPEND NAME_PARTS "${GDE_TARGET_ARCH}") # AKA arch
+
+    if (NOT GDE_CUSTOM_SUFFIX STREQUAL "")
+        list(APPEND NAME_PARTS "${GDE_CUSTOM_SUFFIX}")
+    endif ()
+
+    list(APPEND NAME_PARTS "exe")
+
+
+    list(JOIN NAME_PARTS "." GODOT_EXECUTABLE_NAME)
+    return( PROPAGATE GODOT_EXECUTABLE_NAME )
+endfunction()
+godot_executable_name()
+
+# Output Library Name
+# godot.<platform>.<target>[.dev][.double].<arch>[.custom_suffix][.console].exe
+function( gde_names_gdextension )
+    # Output gdextension library name
+    set(NAME_PARTS ${PROJECT_NAME})
+
+    list(APPEND NAME_PARTS "${GDE_TARGET_PLATFORM}")
+    list(APPEND NAME_PARTS "${GDE_TARGET_PRESET}")
+
+    if (${GEL_DEV_BUILD})
+        list(APPEND NAME_PARTS "dev")
+    endif ()
+
+    if (${GEL_FLOAT_DOUBLE})
         list(APPEND NAME_PARTS "double")
     endif ()
 
@@ -51,7 +95,7 @@ function( gde_generate_names )
 
     list(JOIN NAME_PARTS "." GDE_OUTPUT_NAME)
 
-    return( PROPAGATE GDE_OUTPUT_NAME GDE_TARGET_ARCH GDE_TARGET_PLATFORM )
+    return( PROPAGATE GDE_OUTPUT_NAME )
 endfunction()
 
-gde_generate_names()
+gde_names_gdextension()
