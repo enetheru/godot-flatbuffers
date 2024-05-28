efdcb4034d5cac44f801eb0158f813ad92c00596
Samuel Nicholas <nicholas.samuel@gmail.com>
2024-05-28 12:00:45 +0930

    deleted research code and added call to flatc to generate files

--- project/test_editor_mods.gd
+++ project/test_editor_mods.gd
@@ -38,33 +38,10 @@ func changes_to_fbs():
 	var rcm : PopupMenu
 	var menus = fbs.find_children( "", "PopupMenu", false, false )
 
-	# This is not reliable as on startup there are no items in any of the menus.
-	#for menu : PopupMenu in menus:
-		#if menu.item_count > 0:
-			#rcm = menu
-
 	# I dont like this, but it appears that the second menu is typically the one
 	rcm = menus[1]
 
-	# Connect the tree signals to something
-	# this is not needed
-	#tree.cell_selected.connect(
-		#func():
-			#var tree_item := tree.get_selected()
-			#print( tree_item.get_text(0) )
-	#)
-
-	# this is not needed
-	#tree.empty_clicked.connect( func( position: Vector2, mouse_button_index: int ):
-		#print( "empty_clicked(%s, %s)" % [position, mouse_button_index] )
-	#)
-
-	tree.item_mouse_selected.connect( func( position: Vector2, mouse_button_index: int ):
-		# Check that we have a *.fbs file
-		# I wrote the below before I found the get_current_path function
-		#var tree_item := tree.get_selected()
-		#var item_text = tree_item.get_text(0)
-		#print( "TreeItem = ", item_text  )
+	tree.item_mouse_selected.connect( func( _position: Vector2, _mouse_button_index: int ):
 		var current_path := EditorInterface.get_current_path()
 		if not current_path.ends_with(".fbs"): return
 		rcm.add_separator()
@@ -72,34 +49,31 @@ func changes_to_fbs():
 		var index = rcm.item_count -1
 		# we have to add something to differentiate us from other menu items.
 		rcm.set_item_metadata(index, "fbs")
-		rcm.add_item
 	)
 
 	# Connect the right click signals to something:
 	rcm.id_pressed.connect( func( id ):
 		var meta = rcm.get_item_metadata( id )
 		if meta != "fbs": return
-		print( "generate was clicked")
 
-		# Double check we have the right filetype.
-		# This is not necessary after I found get_current_path()
-		#var tree_item := tree.get_selected()
-		#var item_text = tree_item.get_text(0)
-		#if not item_text.ends_with(".fbs"): return
+		var settings = EditorInterface.get_editor_settings()
+		var flatc_path : String = settings.get( &"plugin/FlatBuffers/flatc_path")
+		if flatc_path.is_empty():
+			printerr( " Please specify the location of flatc in EditorSettings->Plugins->FlatBuffers")
+			return
 
+		var source_path = ProjectSettings.globalize_path(EditorInterface.get_current_path())
+		var output_path = ProjectSettings.globalize_path(EditorInterface.get_current_directory())
 
-		# We need to know the full path to be able to run flatc on our file.
-		# This is no longer necessary as we have get_current_path
-		#var parent : TreeItem
-		#var path : String = item_text
-		#parent = tree_item.get_parent()
-		#while parent:
-			#path = parent.get_text(0) + "/" + path
-			#parent = parent.get_parent()
-		#print( path )
-		# This gives us a path relative to /res://
+		var args : PackedStringArray = [
+			"-o", output_path,
+			"--gdscript", source_path]
 
-		print( ProjectSettings.globalize_path(EditorInterface.get_current_path()) )
+		var output = []
+		var result = OS.execute( flatc_path, args, output, true )
+		if not output.is_empty():
+			if result: printerr( output )
+			else: print( output )
 
-		# OS.execute( flatc path )
+		EditorInterface.get_resource_filesystem().scan()
 	)
