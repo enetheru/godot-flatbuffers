422deb0af2a00b656b98e6c345226623b92c9b3f
Samuel Nicholas <nicholas.samuel@gmail.com>
2024-08-14 09:34:16 +0930

    added testing for PackedStringArray

--- project/tests/test_string_array.gd
+++ project/tests/test_string_array.gd
@@ -1,36 +1,47 @@
 @tool
 extends EditorScript
 
-const fb = preload('res://fbs_files/FBTestStringArray_generated.gd')
+const fb = preload('res://fbs_files/tests/FBTestStringArray_generated.gd')
 
 var pp := FlatBufferPrinter.new()
 
+var string_array : PackedStringArray = []
 
 func _run() -> void:
 	print("== Test Table Arrays ==")
+	# Setup
 
-	# Long way
-	var builder = FlatBufferBuilder.new()
-
-
-	var string_array : PackedStringArray = []
 	var incrementing : String = ""
 	for i in range(8):
 		#string_offsets.append( builder.create_string( "ABCDEFG%s" % i ) )
 		incrementing += "%s" % i
 		string_array.append( incrementing )
 
-	#var array_offset = builder.create_vector( string_offsets )
+	short_way()
+	long_way()
+
+func short_way():
+	print("# Short Way")
+
+	var builder = FlatBufferBuilder.new()
+	var offset = fb.CreateRootTable(builder, string_array )
+	builder.finish( offset )
+
+	## This must be called after `Finish()`.
+	var buf = builder.to_packed_byte_array()
+	print( " - size:%s\n - data: %s" % [ builder.get_size(), buf] )
+
+	reconstruction( buf )
+
+func long_way():
+	print( "# Long Way")
+	var builder = FlatBufferBuilder.new()
 
 	var string_array_offset = builder.create_PackedStringArray( string_array )
 
 	var root_builder = fb.RootTableBuilder.new( builder )
-
-	# Add the individual members of the root table here
 	root_builder.add_my_strings( string_array_offset )
-
-	var root_offset = root_builder.finish()
-	builder.finish( root_offset )
+	builder.finish( root_builder.finish() )
 
 	## This must be called after `Finish()`.
 	#print("# Final Buffer")
@@ -76,9 +87,14 @@ func _run() -> void:
 
 	var root_table := fb.GetRoot( buf )
 
-	print( "my_strings_count(): ", root_table.my_strings_count() )
-	print( "my_strings_at( 0->%s ): " % root_table.my_strings_count() )
-	for i in root_table.my_strings_count():
+	var strings_size = root_table.my_strings_size()
+	print( "my_strings_size(): ", strings_size )
+	print( "my_strings_at( 0->%s ): " % strings_size )
+	for i in strings_size:
 		print( "\t%s: " % i, root_table.my_strings_at(i) )
 
+
+func reconstruction( buffer : PackedByteArray ):
+	print("# Reconstruction")
+	var root_table := fb.GetRoot( buffer )
 	pp.print( root_table )
