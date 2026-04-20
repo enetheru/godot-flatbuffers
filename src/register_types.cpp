021c2e620d6a4e821c5a4ea74d8ffb3989b7b16b
Aiden Jeffrey <aiden.jeffrey@loroco.tech>
2026-04-20 14:32:34 +0100

    uuid: Remove this from the library

--- src/register_types.cpp
+++ src/register_types.cpp
@@ -8,7 +8,6 @@
 #include <godot_cpp/core/class_db.hpp>
 #include <godot_cpp/godot.hpp>
 
-#include "uuid.hpp"
 #include "flatbuffer.hpp"
 #include "flatbufferbuilder.hpp"
 #include "flatbufferverifier.hpp"
@@ -19,7 +18,6 @@ void initialize_module( const godot::ModuleInitializationLevel p_level ) {
   }
 
   using namespace godot_flatbuffers;
-  godot::ClassDB::register_class< UUID >();
   godot::ClassDB::register_class< FlatBuffer >();
   godot::ClassDB::register_class< FlatBufferBuilder >();
   godot::ClassDB::register_class< FlatBufferVerifier >();
